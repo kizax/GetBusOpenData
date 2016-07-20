2f91428c1c6cd4b4bd52a6b90baa554210182a2c
kizax <kizax211075@gmail.com>
2016-07-20 13:07:22 +0800

    新增index

--- busdb.sql
+++ busdb.sql
@@ -31,5 +31,22 @@ CREATE TABLE busdata (
   PRIMARY KEY (ID)
 )ENGINE = MyISAM ;
 
+ALTER TABLE `busdb`.`busdata` 
+ADD INDEX `datatime` USING BTREE (`datatime` ASC),
+ADD INDEX `providerid` USING BTREE (`providerid` ASC),
+ADD INDEX `providername` USING BTREE (`providername` ASC),
+ADD INDEX `carid` USING BTREE (`carid` ASC),
+ADD INDEX `busid` USING BTREE (`busid` ASC),
+ADD INDEX `routeid` USING BTREE (`routeid` ASC),
+ADD INDEX `routename` USING BTREE (`routename` ASC),
+ADD INDEX `longitude` USING BTREE (`longitude` ASC),
+ADD INDEX `latitude` USING BTREE (`latitude` ASC),
+ADD INDEX `speed` USING BTREE (`speed` ASC),
+ADD INDEX `azimuth` USING BTREE (`azimuth` ASC),
+ADD INDEX `stopid` USING BTREE (`stopid` ASC),
+ADD INDEX `stopLocationName` USING BTREE (`stopLocationName` ASC),
+ADD INDEX `goback` USING BTREE (`goback` ASC);
+
+
 INSERT INTO busdata (datatime,cartype,providerid,providername,carid,busid,dutystatus,busstatus,routeid,routename,goback,longitude,latitude,speed,azimuth,stopid,stopLocationName) VALUES ('2016/2/3 14:13:44',1,1100,'大南汽車',222233972,'078-FR',1,0,157863,'266(北投-市府)',0,121.524755,25.084855,11,185.2,41527,'捷運劍潭站(基河)');
 INSERT INTO busdata (datatime,cartype,providerid,providername,carid,busid,dutystatus,busstatus,routeid,routename,goback,longitude,latitude,speed,azimuth,stopid,stopLocationName) VALUES ('2016/2/3 14:13:45',1,1100,'大南汽車',222233972,'078-FR',1,0,157863,'266(北投-市府)',0,121.524755,25.084855,11,185.2,41527,NULL);
